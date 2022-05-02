5f089d385e635ec98a854cc519946109cd6ddb13
marios <mgeorgak859@gmail.com>
2022-05-02 17:15:17 +0300

    added js file content

---
+++ practise.js
@@ -0,0 +1,8 @@
+let football
+let basketball
+let games
+football = "playing football is my hobby";
+basketball = "playing basketball is my hobby";
+games = "playing online games is my hobby";
+let hobbies = football + basketball + games ;
+console.log(hobbies) ;
