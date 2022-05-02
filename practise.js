b3de39d79e448236339177131c8d4f011bb89c3c
marios <mgeorgak859@gmail.com>
2022-05-02 18:49:03 +0300

    some changes to js

--- practise.js
+++ practise.js
@@ -1,8 +1,14 @@
-let football
+let football = "playing football is my hobby.";
 let basketball
 let games
-football = "playing football is my hobby";
-basketball = "playing basketball is my hobby";
-games = "playing online games is my hobby";
-let hobbies = football + basketball + games ;
+basketball = "playing basketball is my hobby.";
+games = "playing online games is my hobby.";
+let hobbies = football + " " + basketball + " " + games ;
 console.log(hobbies) ;
+// Single line comment
+/*
+
+
+*/
+
+let numberTwoAsAString = "two"
